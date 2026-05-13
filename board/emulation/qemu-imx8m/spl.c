8c296ccc9ba82c066dca6b4dfe8d7191ca5765a8
Bernhard Beschow <shentey@gmail.com>
2026-05-13 17:06:44 +0200

    Revert "base"
    This reverts commit 8f6975f4f41286fece4b112a2654ac9318f49310.

--- board/emulation/qemu-imx8m/spl.c
+++ board/emulation/qemu-imx8m/spl.c
@@ -4,7 +4,6 @@
  *
  */
 
-#include "asm-generic/sections.h"
 #include <hang.h>
 #include <init.h>
 #include <log.h>
@@ -100,13 +99,6 @@ int board_fit_config_name_match(const char *name)
 void board_init_f(ulong dummy)
 {
 	int ret;
-
-    char *dst = __image_copy_start;
-    char *src = _image_binary_end -(__image_copy_end - __image_copy_start);
-    char *end = __image_copy_end;
-
-    while (dst < end)
-        *dst++ = *src++;
 
 	arch_cpu_init();
 
